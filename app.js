bd79123cb81385eb4bfac1192f85e65d1e33b71d
Kari Matthews <karimatthews3@gmail.com>
2024-06-15 09:59:25 +1000

    Show scientific and common name on button click

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 
-fetch('https://inaturalist.org/observations.json?has[]=photos&has[]=geoper_page=1')
+fetch('https://inaturalist.org/observations.json?has[]=photos&has[]=geo&qualitygrade=research&per_page=1')
   .then(response => response.json())
   .then(data => {
+    console.log(data)
     const div = document.getElementById('photo');
     const img = document.createElement('img');
 
     img.src = data[0].photos[0].medium_url;
     div.appendChild(img);
+
+
+
+    document.getElementById('scientific_name').textContent = data[0].taxon.name;
+
+    document.getElementById('common_name').textContent = data[0].taxon.common_name.name;
+
+    document.getElementById('myButton').addEventListener('click', function () {
+      document.getElementById('answer').style.display = 'block';
+      this.textContent = 'Answer:';
+    });
   })
   .catch(error => {
     console.error('Error:', error);
