35adc595816a317f52731ff84dadf056099170dd
Kari Matthews <karimatthews3@gmail.com>
2024-06-15 10:28:33 +1000

    Loop through a list

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 
-fetch('https://inaturalist.org/observations.json?has[]=photos&has[]=geo&qualitygrade=research&per_page=1')
+fetch('https://inaturalist.org/observations.json?has[]=photos&has[]=geo&qualitygrade=research&per_page=10')
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    const div = document.getElementById('photo');
-    const img = document.createElement('img');
+
+    // Set initial values to first observation in list
+    let img = document.getElementById('myImg');
 
     img.src = data[0].photos[0].medium_url;
-    div.appendChild(img);
-
-
+    img.onload = function () {
+      // Show the Show Answer button
+      document.getElementById('showAnswerButton').style.display = 'block'
+    };
 
     document.getElementById('scientific_name').textContent = data[0].taxon.name;
-
     document.getElementById('common_name').textContent = data[0].taxon.common_name.name;
 
-    document.getElementById('myButton').addEventListener('click', function () {
+    let currentIndex = 0;
+
+    // Show answers on button click
+    document.getElementById('showAnswerButton').addEventListener('click', function () {
       document.getElementById('answer').style.display = 'block';
-      this.textContent = 'Answer:';
+      this.style.display = 'none';
+      document.getElementById('nextButton').style.display = 'block'
+    });
+
+    document.getElementById('nextButton').addEventListener('click', function () {
+      currentIndex++;
+
+      // Hide the answers
+      document.getElementById('answer').style.display = 'none';
+      document.getElementById('nextButton').style.display = 'none'
+
+      if (currentIndex >= data.length) {
+        currentIndex = 0; // Loop back to the first image when reaching the end
+      }
+
+      let img = document.getElementById('myImg');
+      img.src = data[currentIndex].photos[0].medium_url;
+      document.getElementById('scientific_name').textContent = data[currentIndex].taxon.name;
+      document.getElementById('common_name').textContent = data[currentIndex].taxon?.common_name?.name;
+
+      img.onload = function () {
+        // Show the Show Answer button after the image loads
+        document.getElementById('showAnswerButton').style.display = 'block'
+      };
+
     });
   })
   .catch(error => {
